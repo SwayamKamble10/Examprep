edd66ec50c6773dedb048f03b50bae740b4c0a13
swayam kamble <kambleswayam15@gmail.com>
2025-10-20 15:30:55 +0000

    can we export the code of the app for github

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,19 +22,19 @@ export default function RootLayout({
         <link href="https://fonts.googleapis.com/css2?family=PT+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet" />
         <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:ital,wght@0,400..900;1,400..900&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased" suppressHydrationWarning>
-        <ThemeProvider
+      <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
-        >
-          <FirebaseClientProvider>
-              {children}
-          </FirebaseClientProvider>
-          <Toaster />
-        </ThemeProvider>
-      </body>
+      >
+        <body className="font-body antialiased" suppressHydrationWarning>
+            <FirebaseClientProvider>
+                {children}
+            </FirebaseClientProvider>
+            <Toaster />
+        </body>
+      </ThemeProvider>
     </html>
   );
 }
